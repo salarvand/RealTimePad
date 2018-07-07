d27a4f821e6bc7a5a62c587bc87a6f5101699e7c
salar <asalarvand@gmail.com>
2018-07-07 10:59:39 +0430

    Add project files.

---
+++ RealTimePad/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RealTimePad.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
